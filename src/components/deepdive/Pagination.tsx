a54bae522cdedd808f4521be2408b81be4423362
Adeyemo Ademola <adeyemoademola12@gmail.com>
2025-03-20 12:28:01 +0100

    `Added navigation arrows to pagination component`

--- src/components/deepdive/Pagination.tsx
+++ src/components/deepdive/Pagination.tsx
@@ -1,11 +1,15 @@
 "use client";
+import { FiArrowLeft } from "react-icons/fi";
+import { FiArrowRight } from "react-icons/fi";
 
 export default function Home() {
   return (
     <>
       <main>
-        <section className="w-full h-20 text-center flex justify-center items-center rounded-b-[8px] border border-[#EAECF0]">
-          Page 1 of 10
+        <section className="w-full h-20 text-center flex justify-between px-4 items-center rounded-b-[8px] border border-[#EAECF0]">
+          <FiArrowLeft size={24} className="cursor-pointer text-[#667085]" />
+          <p className="text-[#344054]"> Page 1 of 10</p>
+          <FiArrowRight size={24} className="cursor-pointer text-[#667085]" />
         </section>
       </main>
     </>
