4c0e8c39e81bf8518f51aaaef7722a2fac29343c
Adeyemo Ademola <adeyemoademola12@gmail.com>
2025-03-24 17:25:58 +0100

    `feat: updated page layout and components in src/app/test/page.tsx`

---
+++ src/app/parlay/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SideBar from "@/components/deepdive/Sidebar";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <SideBar />
+      </main>
+    </>
+  );
+}
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,30 +1,140 @@
 "use client";
 
-import Navbar from "@/components/shared/Navbar";
-import Hero from "@/components/terms-and-conditions/Hero";
-import Banner from "@/components/shared/Banner";
-import Footer from "@/components/shared/Footer";
+import SideBar from "@/components/deepdive/Sidebar";
+import Image from "next/image";
 
-export default function TermsAndConditions() {
+export default function Home() {
   return (
     <>
       <main>
-        <Navbar />
-        <section className="bg-[#ECFDF3] md:pt-10 md:pb-[107px] md:px-20 pt-17.5 px-5 pb-33 m-auto text-center">
-          <div className="flex flex-col gap-y-5 md:gap-y-7.5">
-            <h1 className="text-[#101828] md:text-[3.75rem] md:leading-18.5 tracking-[-2%] font-bold gilroy-bold text-[2.25rem] leading-11">
-              Terms & Conditions
-            </h1>
-            <h3 className="text-[#475467] md:text-[1.125rem] md:font-medium md:leading-7.5 leading-6">
-              Version 1.1 September 2024
-            </h3>
-          </div>
-        </section>
+        {/* <SideBar /> */}
 
-        {/*  */}
-        <Hero />
-        <Banner />
-        <Footer />
+        {/* Navbar Section */}
+        <section></section>
+
+        {/* Bracket Section */}
+        <section className="flex flex-col gap-y-3 ml-10 my-10">
+          <div className="w-[243px] min-h-[81px] rounded border border-[#E4E7EC]">
+            <div className="flex items-center justify-between border border-[#E4E7EC]">
+              <div className="border border-[#E4E7EC] w-fit rounded-tl-[5px]">
+                <Image
+                  src="/images/newOrleans.svg"
+                  width={40}
+                  height={40}
+                  alt="New Orleans Logo"
+                  className=""
+                />
+              </div>
+              <p className="text-[.75rem] text-[#000000] leading-4.5">
+                New Orleans Pelicans
+              </p>
+              <div className="bg-[#A6F4C5] w-10 h-10 text-xl leading-6 text-[#667085] px-4 py-2 rounded-tr-[5px]">
+                1
+              </div>
+            </div>
+            <div className="flex items-center justify-between border border-[#E4E7EC]">
+              <div className="border border-[#E4E7EC] w-fit rounded-bl-[5px]">
+                <Image
+                  src="/images/newOrleans.svg"
+                  width={40}
+                  height={40}
+                  alt="New Orleans Logo"
+                  className=""
+                />
+              </div>
+              <p className="text-[.75rem] text-[#000000] leading-4.5">
+                New Orleans Pelicans
+              </p>
+              <div className="bg-[#06543C] w-10 h-10 text-xl leading-6 text-white px-4 py-2 rounded-br-[5px]">
+                1
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#D0D0D0B2] w-[200px] absolute h-[2px] left-70 top-[84px]">
+            <div className="h-[8px] w-[2px] bg-[#D0D0D0B2] relative left-50 top-0"></div>
+          </div>
+
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          <div className="w-[243px] min-h-[81px] rounded border border-[#E4E7EC]">
+            <div className="flex items-center justify-between border border-[#E4E7EC]">
+              <div className="border border-[#E4E7EC] w-fit rounded-tl-[5px]">
+                <Image
+                  src="/images/newOrleans.svg"
+                  width={40}
+                  height={40}
+                  alt="New Orleans Logo"
+                  className=""
+                />
+              </div>
+              <p className="text-[.75rem] text-[#000000] leading-4.5">
+                New Orleans Pelicans
+              </p>
+              <div className="bg-[#A6F4C5] w-10 h-10 text-xl leading-6 text-[#667085] px-4 py-2 rounded-tr-[5px]">
+                1
+              </div>
+            </div>
+            <div className="flex items-center justify-between border border-[#E4E7EC]">
+              <div className="border border-[#E4E7EC] w-fit rounded-bl-[5px]">
+                <Image
+                  src="/images/newOrleans.svg"
+                  width={40}
+                  height={40}
+                  alt="New Orleans Logo"
+                  className=""
+                />
+              </div>
+              <p className="text-[.75rem] text-[#000000] leading-4.5">
+                New Orleans Pelicans
+              </p>
+              <div className="bg-[#06543C] w-10 h-10 text-xl leading-6 text-white px-4 py-2 rounded-br-[5px]">
+                1
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#D0D0D0B2] w-[200px] absolute h-[2px] left-70 top-[186px]">
+            <div className="h-[6px] w-[2px] bg-[#D0D0D0B2] relative left-50 bottom-1"></div>
+          </div>
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          <div className="w-[243px] min-h-[81px] rounded border border-[#E4E7EC] absolute top-[92px] left-90">
+            <div className="flex items-center justify-between border border-[#E4E7EC]">
+              <div className="border border-[#E4E7EC] w-10.5 h-10.5 rounded-tl-[5px]">
+                {/* <Image
+                  src="/images/newOrleans.svg"
+                  width={40}
+                  height={40}
+                  alt="New Orleans Logo"
+                  className=""
+                /> */}
+              </div>
+              <p className="text-[.75rem] text-[#000000] leading-4.5"></p>
+              <div className="bg-[#A6F4C5] w-10 h-10 text-xl leading-6 text-[#667085] px-4 py-2 rounded-tr-[5px]"></div>
+            </div>
+            <div className="flex items-center justify-between border border-[#E4E7EC]">
+              <div className="border border-[#E4E7EC] w-10.5 h-10.5 rounded-bl-[5px]">
+                {/* <Image
+                  src="/images/newOrleans.svg"
+                  width={40}
+                  height={40}
+                  alt="New Orleans Logo"
+                  className=""
+                /> */}
+              </div>
+              <p className="text-[.75rem] text-[#000000] leading-4.5"></p>
+              <div className="bg-[#06543C] w-10 h-10 text-xl leading-6 text-white px-4 py-2 rounded-br-[5px]"></div>
+            </div>
+          </div>
+          {/*  */}
+          {/*  */}
+          {/*  */}
+          {/*  */}
+        </section>
       </main>
     </>
   );
